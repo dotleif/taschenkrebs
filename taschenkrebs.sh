f2af9a7a47eac3f99bef026e7b22931acf2901c6
Frank-Detlef Bockelmann <frank-detlef.bockelmann@hereon.de>
2025-06-26 21:04:09 +0200

    Added taschenkrebs.sh

---
+++ taschenkrebs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# 1) Ensure we pick up your user‐installed console scripts
+export PATH="/gpfs/home/bockelma/.local/bin:$PATH"
+
+# 2) Ensure Python can import from your user site‐packages
+export PYTHONPATH="/gpfs/home/bockelma/.local/lib/python3/site-packages:$PYTHONPATH"
+
+# 3) Go to the script folder
+cd /gpfs/work/bockelma/data/THUENEN/TASCHENKREBS
+
+# 4) Run and log output
+./taschenkrebs.py >> taschenkrebs.log 2>&1
